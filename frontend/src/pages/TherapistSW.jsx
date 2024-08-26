b2638238bb27548a839f82e562814e48993fbbd6
Sanskar Wakchaure <sanskarwakchuare7@gmail.com>
2024-08-26 18:25:08 +0530

    profile and todo Lisr added1

--- frontend/src/pages/TherapistSW.jsx
+++ frontend/src/pages/TherapistSW.jsx
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from 'react';
-// import Card from '../components/PCardSW.jsx';  // Ensure this is used or remove if not needed
-import Calendar from '../components/CalendarSW.jsx'
-import Profile from '../components/ProfileSW.jsx'
+import Calendar from '../components/CalendarSW.jsx';
+import Profile from '../components/ProfileSW.jsx';
 import Cards from '../components/CardsSW.jsx';
-import Loader from '../components/LoaderSW.jsx';  // Import the Loader component
+import Loader from '../components/LoaderSW.jsx';
 import TodoList from '../components/TodoSW.jsx';
 
 const App = () => {
@@ -12,32 +11,34 @@ const App = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 1000); // 5 seconds
+    }, 1000);
 
-    return () => clearTimeout(timer); // Cleanup timer on unmount
+    return () => clearTimeout(timer); 
   }, []);
 
   return (
-    <div className='flex h-max bg-[rgb(249,248,240)]'>
+    <div className='flex flex-col lg:flex-row h-full min-h-screen bg-[rgb(249,248,240)]'>
       {loading ? (
-        <Loader />  // Show loader while loading
+        <Loader /> 
       ) : (
         <>
-          <div className='w-3/4 flex flex-col bg-[rgb(249,248,240)]'>
-            <Cards />
-            <Cards />
-            <Cards />
+          <div className='lg:w-3/4 w-full flex flex-col bg-[rgb(249,248,240)] p-4 space-y-4'>
+            {/* Responsive Cards - Adjust to different screen sizes */}
+            <Cards className='w-full'/>
+            <Cards className='w-full'/>
+            <Cards className='w-full'/>
           </div>
-          <div className='w-1/4'>
-            <div className='flex flex-col justify-between'>
-              <div style={{ marginTop: "+22px" ,marginLeft:'+40px'}}>
+          <div className='lg:w-1/4 w-full flex flex-col items-center lg:items-start p-4'>
+            <div className='flex flex-col justify-between w-full space-y-6'>
+              <div className='mt-5 lg:mt-6 ml-0 lg:ml-10'>
                 <Profile />
               </div>
-              <div style={{marginLeft:'55px',marginTop:'50px'}}>
-                <TodoList/>
+              <div className='mt-5 lg:mt-12 ml-0 lg:ml-12 w-full'>
+                <TodoList />
               </div>
-              {/* <div>
-                <Calendar/>
+              {/* Uncomment to add Calendar */}
+              {/* <div className='mt-5 w-full'>
+                <Calendar />
               </div> */}
             </div>
           </div>
