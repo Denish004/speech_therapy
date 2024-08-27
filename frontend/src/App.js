f5d28a57c5bde98341932d52640cac0dc52ccf44
AlinaaBharde <alinaa.bharde.201327@gmail.com>
2024-08-27 22:02:32 +0530

    HomePAGE

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,27 @@
-import React from 'react'
-import Superviser from './pages/SuperviserKD'
-import Therapist from './pages/TherapistSW'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import TherapistSW from './pages/TherapistSW';
+import Home from './pages/LandingPageAB'
+// import PatientCardDS from './components/PatientCardDS.js';
+import DashboardDS from './components/DashboardDS.js';
+import GaugeChartDS from './components/ChartsDS/GaugeChartDS.js';
+import LineChartDS from './components/ChartsDS/LineChartDS.js';
+import SuperviserKD from './pages/SuperviserKD.js'
 const App = () => {
   return (
-    <div>
-      {/* <Therapist/> */}
-      <Superviser/>
-    </div>
-  )
-}
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/therapist" element={<TherapistSW />} />{/* Route for TherapistSW component */}
+          <Route path="/patient" element={<DashboardDS/>}/>
+          <Route path="/supervisor" element={<SuperviserKD/>}/>
+     
+          {/* Add other routes here as needed */}
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
 export default App
