3979c67661c306e0af1471fafeeac2866ae52eda
Sanskar Wakchaure <sanskarwakchuare7@gmail.com>
2024-09-01 15:38:55 +0530

    authentication1

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -10,7 +10,7 @@ export const useSignup = () => {
     setIsLoading(true)
     setError(null)
 
-    const response = await fetch('http://localhost:8080/api/user/signup', {
+    const response = await fetch('https://speech-therapy.onrender.com/api/user/signup', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({ email, password,name,role })
