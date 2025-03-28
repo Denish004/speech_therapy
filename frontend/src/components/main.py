155673f66d25ca813608b3dcb682f10d6881c453
labhansh naik <labhanshvnaik2104@gmail.com>
2025-03-28 18:23:10 +0530

    Update main.py

--- frontend/src/components/main.py
+++ frontend/src/components/main.py
@@ -61,4 +61,4 @@ async def predict(file: UploadFile = File(...)) -> str:
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
 
-# uvicorn main:app --reload --host 0.0.0.0 --port 8000
+# uvicorn main:app --reload --host 0.0.0.0 --port 8000  
